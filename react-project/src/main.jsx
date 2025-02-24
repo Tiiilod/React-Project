54825ef2d0908f99ddc50570d442d619abf102e0
Tiiilod <turky2202@student.abudzar.sch.id>
2025-02-24 13:50:18 +0700

    update

---
+++ react-project/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Axioshop18 from './meeting/project/path.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Axioshop18 />
+  </StrictMode>,
+)
